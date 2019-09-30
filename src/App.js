2f5e35557cf750197c77f975f84cd0c9c17d4425
susanwere <blue.were@yahoo.com>
2019-09-30 12:25:12 +0300

    coming soon page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Countdown from 'react-countdown-now';
+import BackgroundSlider from 'react-background-slider';
+import image1 from '../src/images/bg1.jpg';
+import image2 from '../src/images/bg2.jpg';
+import image3 from '../src/images/bg3.jpg';
+import image4 from '../src/images/bg4.jpg';
+import image5 from '../src/images/bg5.jpeg';
+import image6 from '../src/images/bg6.jpg';
+
+// Renderer callback with condition
+const renderer = ({ days, hours, minutes, seconds }) => {
+    return <div className="counter">
+      <div className="flex-col-c-m size2 counter-ring m-l-15 m-r-15 m-b-20">
+        <span className="counter-no">{days}</span>
+        <span className="counter-text">days</span>
+      </div>
+      <div className="flex-col-c-m size2 counter-ring m-l-15 m-r-15 m-b-20">
+        <span className="counter-no">{hours}</span>
+        <span className="counter-text">hours</span>
+      </div>
+      <div className="flex-col-c-m size2 counter-ring m-l-15 m-r-15 m-b-20">
+        <span className="counter-no">{minutes}</span>
+        <span className="counter-text">minutes</span>
+      </div>
+      <div className="flex-col-c-m size2 counter-ring m-l-15 m-r-15 m-b-20">
+        <span className="counter-no">{seconds}</span>
+        <span className="counter-text">seconds</span>
+      </div>
+      </div>
+};
 
 function App() {
   return (
     <div className="App">
+      <BackgroundSlider
+        images={[image1, image2, image3, image4, image5, image6]}
+        duration={10} transition={2} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b className="main-text">COMING SOON</b>
+          <br></br>
+          Our website is currently under construction
+          <br></br>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Countdown 
+        date={Date.now() + 5000000000}
+        renderer={renderer} />
+       
       </header>
     </div>
   );
